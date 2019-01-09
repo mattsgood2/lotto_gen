2d7476be336f6936801236c447518954fcd97e6c
Matthew Goodman <mattsgood2@aol.com>
2019-01-09 12:58:35 +0000

    adding lucky stars

--- lotto_checking.py
+++ lotto_checking.py
@@ -4,6 +4,7 @@ import sys
 # need to add lucky stars to the end of the list, maybe with extend.
 
 main_numbers_list = []
+stars_numbers_list = []
 main_numbers = list(range(1, 51))
 stars_numbers = list(range(1,10))
 
@@ -13,23 +14,25 @@ class TheNumbers():
     if input('[Y]es or [N]o  ') == 'y'.lower():
         while True:
             main = random.choice(main_numbers)
+            # stars = random.choice(stars_numbers)
+
             if main not in main_numbers_list:
                 main_numbers_list.append(main)
+                # if stars not in stars_numbers_list:
+                    # stars_numbers_list.append(stars)
+
 
             main_numbers_list.sort()
+            # stars_numbers_list.sort()
 
             if len(main_numbers_list) == 5:
-                print (main_numbers_list)
+                print(main_numbers_list)
+                # if len(stars_numbers_list) == 2:
+                    # print (main_numbers_list, stars_numbers_list)
+                    # print (main_numbers_list)
+
+
                 sys.exit()
-        # print(random.choice(numbers))
-        # print('Working')
-        # sys.exit()
-        #
-    # else:
-    #     print('Thanks for playing See Ya next time')
-
-
-
 
 
 TheNumbers()
