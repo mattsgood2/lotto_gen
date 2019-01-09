bc24ce4a4d0d5d680d60084defdbe0a86ca79610
Matthew Goodman <mattsgood2@aol.com>
2019-01-09 11:25:49 +0000

    re-working lotto numbers

--- lotto_checking.py
+++ lotto_checking.py
@@ -6,19 +6,32 @@ import random
 my_list = []
 numbers = list(range(1, 51))
 
-def checking_my_list():
-    while True:
-        new = random.choice(numbers)
-        if new in my_list:
-            return new
-        elif new not in my_list:
-            my_list.append(new)
+class TheNumbers():
+    print('Would you like to Randomly Gen Euro Lotto numbers ?''\n')
 
-        my_list.sort()
+    if input('[Y]es or [N]o  ') == 'y':
+        print('Working')
+    #     new = random.choice(numbers)
+    #     if new in my_list:
+    #             return new
+    #     elif new not in my_list:
+    #         my_list.append(new)
+    #
+    #     my_list.sort()
+    #
+    #     if len(my_list) == 5:
+    #         print (my_list)
+    #
+    # if input == 'n':
+    #     print('Thanks for playing, See Ya next time')
+    # # elif input == 'n':
+    #
+    #
 
-        if len(my_list) == 5:
-            print (my_list)
 
 
-checking_my_list()
+
+TheNumbers()
 # if already in my_list
+
+# To run program in powershell type python lotto_checking.py
