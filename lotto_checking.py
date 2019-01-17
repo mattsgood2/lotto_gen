193feb3d23338bdfcced628a020525d230c835f3
Matthew Goodman <mattsgood2@aol.com>
2019-01-17 21:55:33 +0000

    added comments as we all need them dont we !

--- lotto_checking.py
+++ lotto_checking.py
@@ -8,9 +8,13 @@ main_numbers = list(range(1, 51))
 stars_numbers = list(range(1,10))
 
 ### Working on this app as i dont trust the lotto lucky dip least this way I know its Truely random ###
+#### Needs alot of re-write due to it being not the best ###
 
+
+### checkes main bumbers are not already within the list if not then prints out ###
 def lotto_num():
-    if input('\nWould you like to Randomly Gen Euro Lotto numbers ? [Y]es or [N]o -->  ') == 'y'.lower():
+
+    if input('Would you like to Randomly Gen Euro Lotto numbers ? [Y]es or [N]o -->  ') == 'y'.lower():
         while True:
             main = random.choice(main_numbers)
 
@@ -23,9 +27,10 @@ def lotto_num():
                 return main_numbers_list
                 break
 
-##checks and randomly gens lucky star Numbers##
+## checks and randomly gens lucky star Numbers ##
 def stars():
-    if input('\nWould You like some Lucky Stars ? --> [Y]es or [N]o ') == 'y'.lower():
+
+    if input('Would You like some Lucky Stars ? --> [Y]es or [N]o ') == 'y'.lower():
         while True:
             star = random.choice(stars_numbers)
 
@@ -37,15 +42,14 @@ def stars():
             if len(stars_numbers_list) == 2:
                 return stars_numbers_list
                 break
-
+### prints all the main numbers and Lucky stars ###
 def printing():
-    print('\nMain Numbers {} \nLucky Stars * {} \n'.format(main_numbers_list, stars_numbers_list))
+    print('Main Numbers {} \nLucky Stars * {} \n'.format(main_numbers_list, stars_numbers_list))
 
-# Game()
+
 lotto_num()
 stars()
 printing()
-# TheNumbers()
 # if already in my_list
 
 # To run program in powershell type python lotto_checking.py
