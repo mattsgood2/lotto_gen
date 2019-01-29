cd5cb5d553e93e2fbd31c953feef2977fd2185fa
Matthew Goodman <mattsgood2@aol.com>
2019-01-29 22:38:22 +0000

    adding Html to email

--- lotto_checking.py
+++ lotto_checking.py
@@ -12,7 +12,6 @@ import smtplib, ssl
 
 
 class lotto:
-
     main_numbers_list = []
     stars_numbers_list = []
     main_numbers = list(range(1, 51))
@@ -83,6 +82,42 @@ These are your Euro numbers. """
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message)
 
+##############################################
+# Adding Html to email with number of lotto
+#############################################
+# message = MIMEMultipart("alternative")
+# message["Subject"] = "multipart test"
+# message["From"] = sender_email
+# message["To"] = receiver_email
+#
+# # Create the plain-text and HTML version of your message
+# text = """\
+# Hi,
+# How are you?
+# Real Python has many great tutorials:
+# www.realpython.com"""
+# html = """\
+# <html>
+#   <body>
+#     <p>Hi,<br>
+#        How are you?<br>
+#        <a href="http://www.realpython.com">Real Python</a>
+#        has many great tutorials.
+#     </p>
+#   </body>
+# </html>
+# """
+#
+# # Turn these into plain/html MIMEText objects
+# part1 = MIMEText(text, "plain")
+# part2 = MIMEText(html, "html")
+#
+# # Add HTML/plain-text parts to MIMEMultipart message
+# # The email client will try to render the last part first
+# message.attach(part1)
+# message.attach(part2)
+
+#############################################
 
 # this allows the app to work under one class, with the above setup def
     def __init__(self):
