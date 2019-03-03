a77062d0798a9db38569902f53618e975d48eb83
Matthew Goodman <mattsgood2@aol.com>
2019-03-03 22:05:22 +0000

    comments updated

--- lotto.py
+++ lotto.py
@@ -21,7 +21,7 @@ class startup:
         # self.start()
         # self.stars()
 
-
+### Generates Lotto Numbers ###
     def playing(self):
         play = input("\nGenerate Euro Number [Y] or [N] > ").lower()
         if play == "y":
@@ -36,6 +36,7 @@ class startup:
             print("\nENTER [Y]es or [N]o ! \n")
             self.playing()
 
+### Checks if You Want Email ###
     def send_email_now(self):
         email = input("Send Email, [Y] or [N] > ")
         if email == "y":
@@ -53,7 +54,7 @@ class startup:
             print(f'{email.upper()} is NOT CORRECT [Y] or [N] !')
             self.send_email_now()
 
-
+### Generates the Main Numbers ###
     def main(self):
         while True:
             main = random.choice(self.main_numbers)
@@ -66,7 +67,7 @@ class startup:
             if len(self.main_numbers_list) == 5:
                 self.stars()
 
-
+### Generates the Lucky Stars ###
     def stars(self):
         while True:
             star = random.choice(self.stars_numbers)
@@ -79,29 +80,33 @@ class startup:
             if len(self.stars_numbers_list) == 2:
                 self.print_nums()
 
-
+### Prints The Numbers Out ###
     def print_nums(self):
         print(f'Main Numbers {str(self.main_numbers_list).strip("[]")} Stars {str(self.stars_numbers_list).strip("[]")}')
         print("\nGOODLUCK !\n")
         self.send_email_now()
         # sys.exit()
 
+### Sends The Email Out ###
     def send_email(self):
-         port = 465
-         smtp_server = "smtp.aol.com"
-         sender_email = os.getenv("MYEMAIL")
-         receiver_email = os.getenv("PIMEMAIL")
-         # receiver_email = input("Enter Email > ")
-         print(f'\nEmail Sent to {receiver_email}  Check Spam ! ')
-         password = os.getenv("PASSWORD")
-         # password = input("Type Your Email Password: ")
+        main_n_str = str(self.main_numbers_list)
+        stars_n_str = str(self.stars_numbers_list)
 
-         message = MIMEMultipart("alternative")
-         message["Subject"] = "Lotto Numbers"
-         message["From"] = sender_email
-         message["To"] = receiver_email
+        port = 465
+        smtp_server = "smtp.aol.com"
+        sender_email = os.getenv("MYEMAIL")
+        receiver_email = os.getenv("PIMEMAIL")
+        # receiver_email = input("Enter Email > ")
+        print(f'\nEmail Sent to {receiver_email}  Check Spam ! \n')
+        password = os.getenv("PASSWORD")
+        # password = input("Type Your Email Password: ")
 
-         html = """\
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Lotto Numbers"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        html = """\
              <html>
                <body>
                  <p>This weeks Lotto Numbers Are <br>
@@ -112,16 +117,16 @@ class startup:
                     How many greats are called Matthew.
                  </p>
                </body>
-             </html> """.format(self.main_numbers_list, self.stars_numbers_list)
+             </html> """.format(main_n_str.strip('[]'), stars_n_str.strip('[]'))
 
-         part1 = MIMEText(html, "html")
-         message.attach(part1)
+        part1 = MIMEText(html, "html")
+        message.attach(part1)
 
-         context = ssl.create_default_context()
-         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-             server.login(sender_email, password)
-             server.sendmail(sender_email, receiver_email, message.as_string())
-             sys.exit()
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            sys.exit()
 
 
     def __init__(self):
